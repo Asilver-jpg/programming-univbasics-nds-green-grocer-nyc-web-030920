4c07cc925a754ebf845808d93edd128c0ab848ef
Alexander Silver <asilver7493@gmail.com>
2020-03-04 22:56:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,7 +12,7 @@ end
 def consolidate_cart(cart)
  arr=[]
  cart.each do |ele|
-   binding.pry
+
     if find_item_by_name_in_collection(ele[:item], arr)
       binding.pry
        find_item_by_name_in_collection(ele[:item], arr)[:count] +=1
